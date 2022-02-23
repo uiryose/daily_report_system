68ae8c9d16477ea806e1c1b391630012b9d453b6
yui <yui@192.168.10.101>
2022-02-23 14:20:15 +0900

    Employeeモデル、EmployeeViewモデル作成

---
+++ src/main/java/actions/views/EmployeeConverter.java
@@ -0,0 +1,119 @@
+package actions.views;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import constants.AttributeConst;
+import constants.JpaConst;
+import models.Employee;
+
+/**
+ * 従業員データのDTOモデル⇔Viewモデルの変換を行うクラス
+ *
+ */
+public class EmployeeConverter {
+
+    /**
+     * ViewモデルのインスタンスからDTOモデルのインスタンスを作成する
+     * @param ev EmployeeViewのインスタンス
+     * @return Employeeのインスタンス
+     */
+
+    public static Employee toModel(EmployeeView ev) {
+
+        //値を代入する変数 = 条件式 ? 変数1 : 変数2;
+        return new Employee(
+                ev.getId(),
+                ev.getCode(),
+                ev.getName(),
+                ev.getPassword(),   //それぞれ、ev.get**しているが具体的に何をgetter経由で取得できるのか？
+
+                ev.getAdminFlag() == null
+                //ev.getAdminFlag()がnullなら、nullを代入
+                        ? null
+                //nullでなければ、次の条件式→adminFlagのgetter発動。管理者フラグ(1:ROLR_ADMIN)から1を取得。getIntegerValue()はreturn this.iを返す
+                        : ev.getAdminFlag() == AttributeConst.ROLE_ADMIN.getIntegerValue()
+                        //adminFlag(0or1)と管理者フラグが一致するなら、ROLE_ADOMIN(1)を代入
+                                ? JpaConst.ROLE_ADMIN
+                        //一致しなければ、ROLE_GENERAL(0)を代入
+                                : JpaConst.ROLE_GENERAL,
+
+                ev.getCreatedAt(),
+                ev.getUpdatedAt(),
+                ev.getDeleteFlag() == null
+                //ev.getDeleteFlag()がnullなら、nullを代入
+                        ? null
+                           //従業員evの削除フラグを取得＝＝削除フラグ(1が設定済み)の数値を取得
+                        : ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()
+                           //削除フラグが一致しなければ、EMP_DEL_TRUE(1)を設定
+                                ? JpaConst.EMP_DEL_TRUE
+                           //一致しなければ、EMP_DEL_FALSE(0)を代入
+                                : JpaConst.EMP_DEL_FALSE);
+        //結局何がreturnされているのか？
+        //ev.getId()はEmployee evのIDを返す。そのIDはどこで設定している？
+
+    }
+
+    /**
+     * DTOモデルのインスタンスからViewモデルのインスタンスを作成する
+     * @param e Employeeのインスタンス
+     * @return EmployeeViewのインスタンス
+     */
+    public static EmployeeView toView(Employee e) {
+
+        if(e == null) {
+            return null;
+        }
+
+        return new EmployeeView(
+                e.getId(),
+                e.getCode(),
+                e.getName(),
+                e.getPassword(),
+                e.getAdminFlag() == null
+                        ? null
+                        : e.getAdminFlag() == JpaConst.ROLE_ADMIN
+                                ? AttributeConst.ROLE_ADMIN.getIntegerValue()
+                                : AttributeConst.ROLE_GENERAL.getIntegerValue(),
+                e.getCreatedAt(),
+                e.getUpdatedAt(),
+                e.getDeleteFlag() == null
+                        ? null
+                        : e.getDeleteFlag() == JpaConst.EMP_DEL_TRUE
+                                ? AttributeConst.DEL_FLAG_TRUE.getIntegerValue()
+                                : AttributeConst.DEL_FLAG_FALSE.getIntegerValue());
+    }
+
+    /**
+     * DTOモデルのリストからViewモデルのリストを作成する
+     * @param list DTOモデルのリスト
+     * @return Viewモデルのリスト
+     */
+    //EmployeeViewクラスは画面に表示する従業員と日付の情報を格納
+    public static List<EmployeeView> toViewList(List<Employee> list){
+      //ざっくりとしたリストをインスタンス化
+        List<EmployeeView> evs = new ArrayList();
+      //既に個別情報が格納されているlistから、toView(e)ごと改めてリストに格納←ここがわからない…
+        for (Employee e : list) {
+            evs.add(toView(e));
+        }
+        return evs;
+    }
+
+    /**
+     * Viewモデルの全フィールドの内容をDTOモデルのフィールドにコピーする
+     * @param e DTOモデル(コピー先)
+     * @param ev Viewモデル(コピー元)
+     */
+    public static void copyViewToModel(Employee e, EmployeeView ev) {
+        e.setId(ev.getId());
+        e.setCode(ev.getCode());
+        e.setName(ev.getName());
+        e.setPassword(ev.getPassword());
+        e.setAdminFlag(ev.getAdminFlag());
+        e.setCreatedAt(ev.getCreatedAt());
+        e.setUpdatedAt(ev.getUpdatedAt());
+        e.setDeleteFlag(ev.getDeleteFlag());
+
+    }
+}
---
+++ src/main/java/models/Employee.java
@@ -0,0 +1,101 @@
+package models;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+import constants.JpaConst;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 従業員データのDTOモデル
+ * DTOモデルはテーブルのデータ構造に沿って作成します
+ */
+
+@Table(name = JpaConst.TABLE_EMP)
+@NamedQueries({
+        @NamedQuery(
+                name = JpaConst.Q_EMP_GET_ALL, //==> ENTITY_EMP + ".getAll" ==> "employee" + ".getAll"
+                query = JpaConst.Q_EMP_GET_ALL_DEF), //==> "SELECT e FROM Employee AS e ORDER BY e.id DESC"
+
+        @NamedQuery(
+                name = JpaConst.Q_EMP_COUNT, //==> ENTITY_EMP + ".count" ==> "employee" + ".count"
+                query = JpaConst.Q_EMP_COUNT_DEF), //==> "SELECT COUNT(e) FROM Employee AS e"
+
+        @NamedQuery(
+                //指定した社員番号を保持する従業員の件数を取得する.指定された社員番号がすでにデータベースに存在しているかを調べます。
+                name = JpaConst.Q_EMP_COUNT_RESISTERED_BY_CODE, //==>ENTITY_EMP + ".countRegisteredByCode" ==>employee +".countRegisteredByCode"
+                query = JpaConst.Q_EMP_COUNT_RESISTERED_BY_CODE_DEF), //==>"SELECT COUNT(e) FROM Employee AS e WHERE e.code = :" + JPQL_PARM_CODE;
+
+        @NamedQuery(
+                //社員番号とハッシュ化済パスワードを条件に未削除の従業員を取得する.
+                //従業員がログインするときに社員番号とパスワードが正しいかをチェックするためのものです。
+                name = JpaConst.Q_EMP_GET_BY_CODE_AND_PASS, //==> ENTITY_EMP + ".getByCodeAndPass" ==> employee + ".getByCodeAndPass"
+                query = JpaConst.Q_EMP_GET_BY_CODE_AND_PASS_DEF)//==>  "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :" + JPQL_PARM_CODE
+})
+
+@Getter //全てのクラスフィールドについてgetterを自動生成する(Lombok)
+@Setter //全てのクラスフィールドについてsetterを自動生成する(Lombok)
+@NoArgsConstructor //引数なしコンストラクタを自動生成する(Lombok)
+@AllArgsConstructor //全てのクラスフィールドを引数にもつ引数ありコンストラクタを自動生成する(Lombok)
+@Entity
+public class Employee {
+
+    @Id
+    @Column(name = JpaConst.EMP_COL_ID) //"id"
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //自動発番
+    private Integer id;
+
+    /**
+     * 社員番号
+     */
+    @Column(name = JpaConst.EMP_COL_CODE, nullable = false, unique = true) //空白・重複不可
+    private String code;
+
+    /**
+     * 氏名
+     */
+    @Column(name = JpaConst.EMP_COL_NAME, nullable = false)
+    private String name;
+
+    /**
+     * パスワード
+     */
+    @Column(name = JpaConst.EMP_COL_PASS, length = 64, nullable = false) //長さ指定
+    private String password;
+
+    /**
+     * 管理者権限があるかどうか（一般：0、管理者：1）
+     */
+    @Column(name = JpaConst.EMP_COL_ADMIN_FLAG, nullable = false)
+    private Integer adminFlag;
+
+    /**
+     *登録日時
+     */
+    @Column(name = JpaConst.EMP_COL_CREATED_AT, nullable = false)
+    private LocalDateTime createdAt;
+
+    /**
+     * 更新日時
+     */
+    @Column(name = JpaConst.EMP_COL_UPDATED_AT, nullable = false)
+    private LocalDateTime updatedAt;
+
+    /**
+     * 削除された従業員かどうか（現役：0、削除済み：1）
+     */
+    @Column(name = JpaConst.EMP_COL_DELETE_FLAG, nullable = false)
+    private Integer deleteFlag;
+
+}
