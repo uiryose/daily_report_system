00c6c6ef36458bf3c18f259c05e770865c87e031
uiryose <ytpaisen@gmail.com>
2022-03-29 16:37:01 +0900

    コメント機能：CommentServiceとCommentValidatorを追加

---
+++ src/main/java/models/validators/CommentValidator.java
@@ -0,0 +1,46 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import actions.views.CommentView;
+import constants.MessageConst;
+
+public class CommentValidator {
+
+
+
+    /**
+     * 日報インスタンスの各項目についてバリデーションを行う
+     * @param rv 日報インスタンス
+     * @return エラーのリスト
+     */
+    public static List<String> validate(CommentView cv){
+
+        List<String> errors = new ArrayList<String>();
+        String contentError = validateContent(cv.getContent());
+
+        //エラーメッセージが返されていたら、リストにエラー文を追加する
+        if (!contentError.equals("")) {
+            errors.add(contentError);
+        }
+
+        //エラーの有無に関わらず、エラー文のリストを返す
+        return errors;
+    }
+
+
+    /**
+     * 内容に入力値があるかをチェックし、入力値がなければエラーメッセージを返却
+     * @param content 内容
+     * @return エラーメッセージ
+     */
+    private static String validateContent(String content) {
+        if(content == null || content.equals("")) {
+            return MessageConst.E_NOCONTENT.getMessage();
+        }
+        //入力値がある場合は空文字を返却
+        return "";
+    }
+
+}
---
+++ src/main/java/services/CommentService.java
@@ -0,0 +1,107 @@
+package services;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import actions.views.CommentConverter;
+import actions.views.CommentView;
+import constants.JpaConst;
+import models.Comment;
+import models.validators.CommentValidator;
+
+public class CommentService extends ServiceBase {
+
+
+    /**
+     * 指定したレポートIDに関わるコメント情報を取得し、Viewリストで返却する
+     * @param id レポートのID
+     * @return
+     */
+    public  List<CommentView> getAllMine(int id){
+
+        List<Comment> comments = em.createNamedQuery(JpaConst.Q_COM_GET_ALL_MINE, Comment.class)
+                .setParameter(JpaConst.JPQL_PARM_ID, id)
+                .getResultList();
+
+        return CommentConverter.toViewList(comments);
+
+    }
+
+
+    /**
+     * 画面から入力されたコメント内容を元にデータを1件作成し、コメントテーブルに登録する
+     * @param cv コメントの内容
+     * @return バリデーションで発生したエラーのリスト
+     */
+    public List<String> create(CommentView cv){
+
+        List<String> errors = CommentValidator.validate(cv);
+
+        if(errors.size() == 0) {
+
+            LocalDateTime ldt = LocalDateTime.now();
+            cv.setCreatedAt(ldt);
+            cv.setUpdatedAt(ldt);
+            createInternal(cv);
+
+        }
+        //バリデーションで発生したエラーを返却（エラーがなければ0件の空リスト）
+        return errors;
+
+    }
+
+
+    /**
+     * 画面から入力されたコメントの登録内容を元に、コメントデータを更新する
+     * @param cv コメントの更新内容
+     * @return バリデーションで発生したエラーのリスト
+     */
+    public List<String> update(CommentView cv){
+
+        List<String> errors = CommentValidator.validate(cv);
+
+        if(errors.size() == 0) {
+
+            LocalDateTime ldt = LocalDateTime.now();
+            cv.setUpdatedAt(ldt);
+            updateInternal(cv);
+        }
+
+        return errors;
+    }
+
+
+    /**
+     * コメントIDを条件にコメントデータを1件取得する
+     * @param id
+     * @return 取得データのインスタンス
+     */
+    private Comment findOneInternal(int id) {
+        return em.find(Comment.class, id);
+    }
+
+
+    /**
+     * コメントデータを1件登録する
+     * @param ev コメントデータ
+     */
+    private void createInternal(CommentView ev) {
+
+        em.getTransaction().begin();
+        em.persist(CommentConverter.toModel(ev));
+        em.getTransaction().commit();
+    }
+
+    /**
+     * コメントデータを1件更新する
+     * @param ev コメントデータ
+     */
+    private void updateInternal(CommentView cv) {
+
+        em.getTransaction().begin();
+        Comment c = findOneInternal(cv.getId());
+        CommentConverter.copyViewToModel(c, cv);
+        em.getTransaction().commit();
+    }
+
+}
